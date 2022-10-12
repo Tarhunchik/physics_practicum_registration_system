02733164fe31a03b3e56d8d228b5d314365b4a55
arist0crab <BondareVarechka@yandex.ru>
2022-10-12 23:00:28 +0300

    New design was improved

--- Application/views.py
+++ Application/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect, HttpResponse, FileResponse, Http404
 from django.shortcuts import render
 from django.contrib.auth import login, logout, authenticate
+from django.template import RequestContext
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 import os
@@ -21,13 +22,13 @@ def get_context_base():
 
 def index_page(request):
     context = get_context_base()
-    context['title'] = 'Main Menu'
+    context['title'] = 'Главная страница'
     return render(request, 'new_index.html', context)
 
 
 def register_page(request):
     context = get_context_base()
-    context['title'] = 'Sign up page'
+    context['title'] = 'Регистрация'
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -63,7 +64,7 @@ def register_page(request):
 
 def login_page(request):
     context = get_context_base()
-    context['title'] = 'Login page'
+    context['title'] = 'Вход в аккаунт'
     user = request.user
     if user.is_authenticated:
         return HttpResponseRedirect('/main')
@@ -89,20 +90,23 @@ def logout_view(request):
 
 def test_page(request):
     context = get_context_base()
-    context['title'] = 'Test page'
-    context['grade'] = request.user.grade
+    context['title'] = 'Задачи'
+    if not request.user.is_authenticated:
+        context['grade'] = None
+    else:
+        context['grade'] = request.user.grade
     return render(request, 'new_test.html', context)
 
 
 def non_authorised_user_page(request):
     context = get_context_base()
-    context['title'] = 'Authorise now!'
-    return render(request, 'non_authorised_user_page.html', context)
+    context['title'] = 'Вы не авторизованы'
+    return render(request, 'new_non_authorised_user_page.html', context)
 
 
 def schedule_page1(request):
     context = get_context_base()
-    context['title'] = 'Schedule page'
+    context['title'] = 'Запись'
     if not request.user.is_authenticated:
         return HttpResponseRedirect('/non_auth_user')
     if request.user.is_teacher:
@@ -216,32 +220,14 @@ def schedule_page3(request):
 
 def tg_bot_page(request):
     context = get_context_base()
-    context['title'] = 'Telegram bot page'
-    return render(request, 'telegram_bot.html', context)
+    context['title'] = 'Телеграм бот'
+    return render(request, 'new_telegram_bot.html', context)
 
 
 def contacts_page(request):
     context = get_context_base()
-    context['title'] = 'Contacts page'
-    return render(request, 'contacts.html', context)
-
-
-def rules_page(request):
-    context = get_context_base()
-    context['title'] = 'Rules page'
-    return render(request, 'rules.html', context)
-
-
-def recording_success_page(request):
-    context = get_context_base()
-    context['title'] = 'Congratulations!'
-    return render(request, 'recording_done_successfully.html', context)
-
-
-def recording_error_page(request):
-    context = get_context_base()
-    context['title'] = 'Recoding error page'
-    return render(request, 'recording_is_busy.html', context)
+    context['title'] = 'Наши контакты'
+    return render(request, 'new_contacts.html', context)
 
 
 def account_page(request):
@@ -266,7 +252,7 @@ def account_page(request):
                 task = ['task 1', 'task 2', 'task 3'][int(obj.task) - 1]
                 time = ['12:00 - 14:00', '14:00 - 16:00', '16:00 - 18:00'][int(obj.time) - 1]
                 past_recs.append((task, obj.day, time))
-        context['title'] = f'{request.user.username} account'
+        context['title'] = f'{request.user.username} аккаунт'
         context['name'] = request.user.username
         context['teacher'] = request.user.is_teacher
         context['admin'] = request.user.is_admin
@@ -275,7 +261,12 @@ def account_page(request):
         context['last_name'] = request.user.last_name
         context['cur_recs'] = cur_recs
         context['past_recs'] = past_recs
-        return render(request, 'account.html', context)
+        return render(request, 'new_account.html', context)
 
 
+def error_404(request, exception):
+    return render(request, 'error_404.html')
 
+
+def error_500(request):
+    return render(request, 'error_500.html')
--- Project/urls.py
+++ Project/urls.py
@@ -21,16 +21,18 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index_page),
     path('main/', views.index_page),
-    path('register/', views.register_page),
+    # path('register/', views.register_page),
     path('test/', views.test_page),
-    path('logout/', views.logout_view),
-    path('login/', views.login_page),
+    # path('logout/', views.logout_view),
+    # path('login/', views.login_page),
     path('non_auth_user/', views.non_authorised_user_page),
     path('schedule/1/', views.schedule_page1),
     path('schedule/2/', views.schedule_page2),
     path('schedule/3/', views.schedule_page3, name='schedule/3'),
     path('telegram_bot/', views.tg_bot_page),
     path('contacts/', views.contacts_page),
-    path('rules/', views.rules_page),
-    path('account/', views.account_page),
+    # path('account/', views.account_page),
 ]
+
+handler404 = "Application.views.error_404"
+handler500 = "Application.views.error_500"
