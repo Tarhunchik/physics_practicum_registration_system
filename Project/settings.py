f93246171f3b6817b4140820b51accc13fae6b45
arist0crab <BondareVarechka@yandex.ru>
2022-11-01 14:42:31 +0300

    bugfix

--- Project/settings.py
+++ Project/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'aelaevyt_django',
-        'USER': 'Tarhunchik',
+        'USER': 'aelaevyt_django',
         'PASSWORD': 'Elaev241206',
         'HOST': 'localhost',
     }
